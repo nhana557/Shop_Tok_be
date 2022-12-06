5d5600b77044544924609c976950ab1a525b5d7c
Taryana <118675343+Taryana89@users.noreply.github.com>
2022-12-07 04:33:43 +0700

    fixed: deploy

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -3,17 +3,9 @@ const nodemailer = require('nodemailer')
 // const jwt = require('jsonwebtoken')
 const { google } = require("googleapis");
 const text = require('./templateEmail');
-
-const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.REDIRECT_URI,
-)
-
-oAuth2Client.setCredentials({refresh_token : process.env.GMAIL_REFRESH_TOKEN})
 const sendEmail = async ({email, fullname, token}) =>{
     try {
-        const accessToken = await oAuth2Client.getAccessToken();
+        // const accessToken = await oAuth2Client.getAccessToken();/
         
         const transporter = nodemailer.createTransport({
             service: 'gmail',
