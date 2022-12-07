7e08d7f0fc9d974e8461036cf640a6fcd3e04909
Taryana <118675343+Taryana89@users.noreply.github.com>
2022-12-07 08:39:22 +0700

    fixed: deploy

--- src/controller/products.js
+++ src/controller/products.js
@@ -93,7 +93,6 @@ const productController = {
       id_toko,
       photo: urls.map((url) => `https://drive.google.com/thumbnail?id=${url.id}&sz=s1080`)
     }
-    `https://drive.google.com/thumbnail?id=1iwlpEXg7nik2lXyHA_Oq95JEAu-q98cI&sz=s1080`
     
     insert(data)
       .then(
